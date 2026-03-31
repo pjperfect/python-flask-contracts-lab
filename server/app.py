a9b7b2cc3dfebd8035ec3d3df5148e6c3b8d6886
Philip Olembo <pjole.kenya@gmail.com>
2026-03-31 23:50:44 +0300

    Lab: Routes Request Response Cycle - Managing Contracts

--- server/app.py
+++ server/app.py
@@ -2,9 +2,31 @@
 
 from flask import Flask, request, current_app, g, make_response
 
-contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
-customers = ["bob","bill","john","sarah"]
+contracts = [
+    {"id": 1, "contract_information": "This contract is for John and building a shed"},
+    {"id": 2, "contract_information": "This contract is for a deck for a buisiness"},
+    {
+        "id": 3,
+        "contract_information": "This contract is to confirm ownership of this car",
+    },
+]
+customers = ["bob", "bill", "john", "sarah"]
 app = Flask(__name__)
 
-if __name__ == '__main__':
+@app.route("/contract/<int:contract_id>")
+def get_contract(contract_id):
+    for contract in contracts:
+        if contract["id"] == contract_id:
+            return contract["contract_information"], 200
+    return "Contract not found", 404
+
+
+@app.route("/customer/<customer_name>")
+def get_customer(customer_name):
+    if customer_name in customers:
+        return "", 204
+    return "Customer not found", 404
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
